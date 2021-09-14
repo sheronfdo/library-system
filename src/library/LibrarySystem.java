ab8e8fdf05fad2ca7869b26a7cf25332c9143784
sheronfdo <jamithsheron5@gmail.com>
2021-09-14 18:15:18 +0530

    run home

--- src/library/LibrarySystem.java
+++ src/library/LibrarySystem.java
@@ -15,7 +15,7 @@ public class LibrarySystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new Login().setVisible(true);
+        new Home().setVisible(true);
     }
     
 }
