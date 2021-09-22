00cc17860e2d361fcfb2fcacbd9aa28b74d48532
sheronfdo <jamithsheron5@gmail.com>
2021-09-23 04:17:28 +0530

    user details

---
+++ src/library/UserProfile.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jamit
+ */
+public class UserProfile {
+    static String username;
+    static int empID;
+    static String name;
+    static String address;
+    static String telephoneNo;
+    static String nic;
+    static String gender;
+    static Date dob;
+    static int posID;
+    static String userPosition;
+    static boolean employee;
+    static boolean member;
+    static boolean author;
+    static boolean publisher;
+    static boolean positionManage;
+    static boolean book;
+    static boolean checkout;
+    static boolean returnManage;
+    static boolean renew;
+    
+    
+}
+
